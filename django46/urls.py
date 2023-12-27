83c10d9b2955f406a0e4f33a06bc3abc4157ede9
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-12-27 19:02:16 +0500

    new changes

--- django46/urls.py
+++ django46/urls.py
@@ -19,13 +19,15 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from products.views import home_page, product_page, get_product_category_page
+from products.views import home_page, get_product_category_page, MyLoginView, logout_view, search_product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page),
-    path('shop/', product_page),
-    path('shop/<int:pk>', get_product_category_page),
+    path('', home_page, name='home'),
+    path('shop/<int:pk>', get_product_category_page, name='category_page'),
+    path('login', MyLoginView.as_view(), name='login'),
+    path('logout', logout_view, name='logout'),
+    path('search', search_product, name='search-product'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
